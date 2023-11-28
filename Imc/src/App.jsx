7cb592e8a79d4822df2470f9e8f5dd24d2577707
Matheus Pacheco Cruz <pachecx35@gmail.com>
2023-11-28 17:56:08 -0300

    Finalizando projeto

--- Imc/src/App.jsx
+++ Imc/src/App.jsx
@@ -5,22 +5,56 @@ import { data } from './data/data';
 import { ImcTable } from './components/ImcTable';
 
 function App() {
+
+  const [imc, setImc] = useState("");
+  const [info, setInfo] = useState("");
+  const [infoClass, setInfoClass] = useState("");
+  
   const calcImc = (e, height, weight) => {
-    e.preventDefault()
+    e.preventDefault() 
 
     if(!weight || !height) return;
 
-    console.log(height, weight)
-    console.log("Executou")
+    const weightFloat = +weight.replace(",", ".");
+    const heightFloat = +height.replace(",", ".");
+
+    const imcResult = (weightFloat / (heightFloat * heightFloat)).toFixed(1);
+
+    setImc(imcResult)
+
+    data.forEach((item) => {
+      if(imcResult >= item.min && imcResult <= item.max){
+        setInfo(item.info)
+        setInfoClass(item.infoClass)
+      }
+    });
+
+    if(!info) return;
   };
 
-  const [imc, setImc] = useState("")
-  const [info, setInfo] = useState("")
-  const [infoClass, setInfoClass] = useState("")
+  const resetCalc = (e)=>{
+    e.preventDefault();
+
+    setImc("");
+    setInfo("");
+    setInfoClass("");
+  };
+
+
   return (
     
     <div className='container'>
-      {!imc ? <ImcCalc calcImc={calcImc}/> : <ImcTable />  }
+      {!imc ? (
+        <ImcCalc calcImc={calcImc}/> 
+        ) : (
+        <ImcTable 
+          data={data} 
+          imc={imc} 
+          info={info} 
+          resetCalc={resetCalc}
+          infoClass={infoClass}
+        />  
+      )}
     </div>
   );
 }
--- Imc/src/components/ImcCalc.jsx
+++ Imc/src/components/ImcCalc.jsx
@@ -14,51 +14,53 @@ export function ImcCalc({ calcImc }){
 
     const validDigits = (text) => {
         return text.replace(/[^0-9,]/g, "");
-    }
+    };
 
     const handleHeightChange = (e)=>{
         const updatedValue = validDigits(e.target.value)
         SetHeight(updatedValue)
-    }
+    };
 
-    const handleweighttChange = (e)=>{
-        const updatedValue2 = validDigits(e.target.value)
-        SetHeight(updatedValue2)
-    }
+    const handleWeightChange = (e)=>{
+        const updatedValue = validDigits(e.target.value)
+        SetWeight(updatedValue)
+    };
 
     return(
         <div>
-            <h2>Calculadora IMC</h2>
-            <form id='imc-form'>
-                <div className="form-inputs">
+            <div id="calc-container">
+                <h2>Calculadora IMC</h2>
+                <form id='imc-form'>
+                    <div className="form-inputs">
+                        <div className="form-control">
+                            <label htmlFor='height'>Altura:</label>
+                            <input 
+                                type='text'
+                                name='height'
+                                id='height'
+                                placeholder='Exemplo 1,75'
+                                onChange={(e) =>  handleHeightChange(e)}
+                                value={height}
+                            />
+                        </div>
+                    </div>
                     <div className="form-control">
-                        <label htmlFor='height'>Altura:</label>
+                        <label htmlFor='weight'>Peso:</label>
                         <input 
                             type='text'
-                            name='height'
-                            id='height'
-                            placeholder='Exemplo 1,75'
-                            onChange={(e) =>  handleHeightChange(e)}
-                            value={height}
+                            name='weight'
+                            id='weight'
+                            placeholder='Exemplo 78'
+                            onChange={(e) => handleWeightChange(e)}
+                            value={weight}
                         />
                     </div>
-                </div>
-                <div className="form-control">
-                    <label htmlFor='weight'>Peso:</label>
-                    <input 
-                        type='text'
-                        name='weight'
-                        id='weight'
-                        placeholder='Exemplo 78'
-                        onChange={(e) => handleweighttChange(e)}
-                        value={weight}
-                    />
-                </div>
-                <div className="action-control">
-                    <Button id="calc-btn" text="Calcular" action={(e) => calcImc(e, height, weight)}/>
-                    <Button id="clear-btn" text="limpar" action={clearForm}/>
-                </div>
-            </form>
+                    <div className="action-control">
+                        <Button id="calc-btn" text="Calcular" action={(e) => calcImc(e, height, weight)}/>
+                        <Button id="clear-btn" text="limpar" action={clearForm}/>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 }
